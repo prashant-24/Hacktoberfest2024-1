20c450825541d9b897bf6569cbb2bd5401495103
prashant-24 <47084260+prashant-24@users.noreply.github.com>
2024-10-09 12:28:54 +0530

    Create Find_GCD.cpp

---
+++ C++_Problems/Find_GCD.cpp
@@ -0,0 +1,32 @@
+// C++ program to find GCD of two numbers using simple
+// arithmetic
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    // Find Minimum of a and b
+    int res = min(a, b);
+
+      // Testing divisiblity with all numbers starting from
+    // min(a, b) to 1
+
+    while (res > 1) {
+
+        // If any number divide both a and b, so we
+        // got the answer
+        if (a % res == 0 && b % res == 0)
+            break;
+        res--;
+    }
+    return res;
+}
+
+int main() {
+    int a = 12, b = 16;
+  
+    // Finding gcd of two numbers a and b
+
+
+    cout << gcd(a, b);
+    return 0;
+}
